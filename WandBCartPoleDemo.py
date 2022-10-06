0537b22bb41bf220a683cd9dcf6f82c9bd927ea3
Dean <deanvanaswegen.dva@gmail.com>
2022-10-06 13:14:36 +0200

    task uncomment

--- WandBCartPoleDemo.py
+++ WandBCartPoleDemo.py
@@ -9,7 +9,7 @@ import wandb
 from wandb.integration.sb3 import WandbCallback
 import torch
 
-#from clearml import Task
+from clearml import Task
 task = Task.init(project_name='Test', task_name='CartPole1')
 task.set_base_docker('nvidia-cuda-11.8.0-cudnn8-runtime-ubuntu20.04')
 task.execute_remotely(queue_name="3090 Queue")#,docker_image="11.8.0-cudnn8-runtime-ubuntu20.04")pytorch/pytorch:1.7.1-cuda11.0-cudnn8-runtime
